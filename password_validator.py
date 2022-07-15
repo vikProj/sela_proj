cfbff169554022f58aba244189a97b4fd9621908
victoria <datasiencework@gmail.com>
2022-07-15 17:55:55 +0300

    Password validation script now reads password from passwprd.txt file in json format

--- password_validator.py
+++ password_validator.py
@@ -1,3 +1,4 @@
+import json
 import re
 import sys
 
@@ -13,7 +14,6 @@ def validate_password(psw: str) -> (bool, List[str]):
         *2. Contain both alphabet and number. \n
         *3. Include both the small and capital case letters. \n
 
-    :return:
     :param psw: str - Provided password
     :return: tuple of:
         1. flag - True/False for Valid/Invalid password.
@@ -40,14 +40,14 @@ def validate_password(psw: str) -> (bool, List[str]):
     return flag, msgs
 
 
-def print_func(validated_flag: bool, msgs: list):
+def print_func(validate_flag: bool, msgs: list):
     """
     This method prints provided messages in green when provided flag is True and in red when provided flag is Red
-    :param validated_flag: bool
+    :param validate_flag: bool
     :param msgs: list of messages to print
     :return:
     """
-    if validated_flag:
+    if validate_flag:
         pr_green("Provided password is valid!")
     else:
         pr_red("Provided password is not valid!\n")
@@ -75,21 +75,68 @@ def pr_red(text: str):
     print(f"\033[91m {text}\033[00m")
 
 
+def read_file_key(file_name: str, key: str) -> (str, str):
+
+    """
+    This method reads key from provided file name. File name should be in json format. \n
+
+    :param file_name: provided file name in json format to read
+    :param key: a key to retrieve from a file
+    :return: tuple of:
+        1. password - the read value of the password from the file.
+        2. messages - message in case of failure of file reading
+
+    """
+
+    msg = ""
+    variables = {}
+    psw = ""
+
+    try:
+        with open(file_name) as f:
+            variables = json.load(f)
+
+    except OSError as err:
+        msg = f"Can't read provided file {file_name},\n {err}"
+
+    try:
+        if variables:
+            psw = variables[key]
+
+    except KeyError as e:
+        msg = f"Can't read password from provided file"
+
+    return psw, msg
+
+
 def main():
     """
-    This method retrieves password from provided input arguments and invokes password validation functionality.
-    Initialize exit code for 0 in case of valid password and exit code for 1 in case of invalid password.
+    This method retrieves provided file name as input arguments and invokes password validation functionality. \n
+    Please provide argument and run script in following way: \n
+    python ./password_validator.py -f '/mypath/password.txt'\n
 
-    Color the output green if it passed the validation and red if it didn’t.
+    File should be provided in json format. \n
+    Initialize exit code for 0 in case of valid password and exit code for 1 in case of invalid password. \n
 
-    :return: None
-    """
-    if len(sys.argv) < 2:
-        pr_red("Password is not provided")
+    Color the output green if it passed the validation and red if it didn’t. \n
+
+    :return: exit code for 0 in case of valid password and exit code for 1 in case of invalid password
+        """
+
+    if len(sys.argv) < 3 or sys.argv[1] != "-f":
+        pr_red("File Name is not provided. Please provide next argument -f 'fileName'")
         sys.exit(1)
 
-    psw = sys.argv[1]
-    flag, msgs = validate_password(psw)
+    file_name = sys.argv[2]
+    key = "password"
+
+    password, msg = read_file_key(file_name, key)
+
+    if msg:
+        pr_red(msg)
+        sys.exit(1)
+
+    flag, msgs = validate_password(password)
 
     print_func(flag, msgs)
 
@@ -101,7 +148,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
